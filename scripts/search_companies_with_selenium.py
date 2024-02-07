29d5df6eef252bb6a3728a81ce21fc4ed361b5e5
andromedalactea <bry3639@gmil.com>
2024-02-06 20:43:40 -0500

    [REFACTOR] main script to english

--- scripts/search_companies_with_selenium.py
+++ scripts/search_companies_with_selenium.py
@@ -3,33 +3,40 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import NoSuchElementException, ElementClickInterceptedException
+from fake_useragent import UserAgent
 import csv
 import time
+import random
 
-def save_company_links_to_csv_selenium(keyword, num_links, filename='output_files/companies_links_japan.csv', driver_path='C:/path/to/chromedriver.exe'):
-    # Configura Selenium con el path al ChromeDriver
+def save_company_links_to_csv_selenium(keyword, num_links, filename='output_files/companies_links.csv', driver_path='C:/path/to/chromedriver.exe'):
+    # Generates a random User-Agent
+    ua = UserAgent()
+    user_agent = ua.random
+    
+    # Sets Selenium options to use the generated User-Agent
+    options = webdriver.ChromeOptions()
+    options.add_argument(f"user-agent={user_agent}")
+    
+    # Configures Selenium with the path to ChromeDriver and the defined options
     service = Service(executable_path=driver_path)
-    driver = webdriver.Chrome(service=service)
+    driver = webdriver.Chrome(service=service, options=options)
     
-    # Construye la URL de búsqueda
-    # Construye la URL de búsqueda incluyendo los parámetros para desactivar el filtrado y la personalización
-    search_query = f"{keyword} company or factory"
+    # Builds the search URL including parameters to disable personalization and filtering
+    search_query = f"{keyword} companies or factories"
     base_url = "https://www.google.com/search?q="
-    # Añade &filter=0&num=100&pws=0 para incluir resultados omitidos y desactivar personalización
-    search_url = base_url + search_query.replace(" ", "+") + "&filter=0&num=100&pws=0"
-
+    search_url = base_url + search_query.replace(" ", "+") + "&filter=0&num=2000&pws=0"
     
-    # Accede a la página de búsqueda
+    # Accesses the search page
     driver.get(search_url)
     
-    # Espera inicial para que la página cargue completamente
-    time.sleep(0.5)
+    # Initial wait for the page to fully load
+    # time.sleep(0.2)
     
     links_collected = []
     attempts = 0
 
     while len(links_collected) < num_links and attempts < 10:
-        # Encuentra y almacena los enlaces actuales
+        # Finds and stores the current links
         result_divs = driver.find_elements(By.CSS_SELECTOR, "div.g")
         for result_div in result_divs:
             anchor = result_div.find_elements(By.CSS_SELECTOR, "a")
@@ -40,40 +47,36 @@ def save_company_links_to_csv_selenium(keyword, num_links, filename='output_file
                     if len(links_collected) >= num_links:
                         break
         
-        # Desplazamiento hacia abajo
+        # Scrolls down
         driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-        time.sleep(0.2)  # Espera a que carguen más resultados
+        # time.sleep(0.1)  # Waits for more results to load
         
-        # Intenta hacer clic en el botón "Más resultados" si está presente
+        # Attempts to click the "More results" button if present
         try:
-            more_results_button = driver.find_element(By.XPATH, "//a[contains(@aria-label, 'Más resultados')]")
+            more_results_button = driver.find_element(By.XPATH, "//a[contains(@aria-label, 'More results')]")
             driver.execute_script("arguments[0].click();", more_results_button)
-            time.sleep(0.2)  # Espera a que carguen los resultados adicionales
+            # time.sleep(0.1)  # Waits for additional results to load
         except (NoSuchElementException, ElementClickInterceptedException):
-            print("No se encontró el botón 'Más resultados' o no se pudo hacer clic en él, intentando de nuevo...")
+            print("Trying again...")
             attempts += 1
         
-    # Escribe los enlaces en el archivo CSV
+    # Writes the links to the CSV file
     with open(filename, mode='w', newline='', encoding='utf-8') as file:
         writer = csv.writer(file)
         writer.writerow(['Keyword', 'URL'])
         for link in links_collected:
             writer.writerow([keyword, link])
     
-    # Cierra el navegador
+    # Closes the browser
     driver.quit()
 
-
-
-
-
-# Ejemplo de uso
+# Example usage
 keyword = "clean"
 num_links = 100
-driver_path = 'ignore_files/chromedriver'  # Asegúrate de ajustar esto a la ubicación de tu ChromeDriver
+driver_path = 'ignore_files/chromedriver'  # Make sure to adjust this to your ChromeDriver location
 
 start_time = time.time()
 save_company_links_to_csv_selenium(keyword, num_links, driver_path=driver_path)
 end_time = time.time()
 total_time = end_time - start_time
-print(f"Tiempo total de ejecución: {total_time} segundos.")
+print(f"Total execution time: {total_time} seconds.")
